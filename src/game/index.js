f42bded1a38db99fbbae4d1e929d17d982d7a71e
Michael Braun <m.braun110@gmail.com>
2019-10-31 20:20:14 +0100

    [master] added end condition: if the whole field is marked but no one won

--- src/game/index.js
+++ src/game/index.js
@@ -21,14 +21,17 @@ Game.prototype.mark = function(player, field) {
     let status = this.playground.fields[position].mark(marker[player]);
 
     if(!status.error) {
-        let end = false;
+        let end = endGame(this, player, position);
 
-        if(endGame(this, player, position)) {
-            end = true;
+        if(end === 1) {
             console.log('Player ' + player + ' has won!');
+
+            return { error: false, endGame: true, winner: player };
+        } else if(end === 2) {
+            return { error: false, endGame: true, winner: 'Draw' };
         }
 
-        return { error: false, endGame: end };
+
     }
 
     return { error: true }
@@ -36,105 +39,125 @@ Game.prototype.mark = function(player, field) {
 
 module.exports = Game;
 
+//executes endgame conditions
 function endGame(game, player, field) {
     switch (field) {
         case 0:
             if(check(game, player, 1) && check(game, player, 2)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 3) && check(game, player, 6)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 4) && check(game, player, 8)) {
-                return true;
+                return 1;
             }
             break;
         case 1:
             if(check(game, player, 0) && check(game, player, 2)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 4) && check(game, player, 7)) {
-                return true;
+                return 1;
             }
             break;
         case 2:
             if(check(game, player, 1) && check(game, player, 0)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 5) && check(game, player, 8)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 4) && check(game, player, 6)) {
-                return true;
+                return 1;
             }
             break;
         case 3:
             if(check(game, player, 4) && check(game, player, 5)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 0) && check(game, player, 6)) {
-                return true;
+                return 1;
             }
             break;
         case 4:
             if(check(game, player, 1) && check(game, player, 7)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 3) && check(game, player, 5)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 2) && check(game, player, 6)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 0) && check(game, player, 8)) {
-                return true;
+                return 1;
             }
             break;
         case 5:
             if(check(game, player, 3) && check(game, player, 4)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 2) && check(game, player, 8)) {
-                return true;
+                return 1;
             }
             break;
         case 6:
             if(check(game, player, 0) && check(game, player, 3)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 7) && check(game, player, 8)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 2) && check(game, player, 4)) {
-                return true;
+                return 1;
             }
             break;
         case 7:
             if(check(game, player, 6) && check(game, player, 8)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 1) && check(game, player, 4)) {
-                return true;
+                return 1;
             }
             break;
         case 8:
             if(check(game, player, 6) && check(game, player, 7)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 2) && check(game, player, 5)) {
-                return true;
+                return 1;
             }
             if(check(game, player, 0) && check(game, player, 4)) {
-                return true;
+                return 1;
             }
             break;
     }
+
+    if(checkFull(game)) {
+        return 2;
+    }
+
+    return 0;
 }
 
+//Checks if a field is already marked
 function check(game, player, field) {
     if(game.playground.fields[field].get() === marker[player]) {
         return true;
     }
 
     return false;
+}
+
+//End condition if the field is full and there is no winner
+function checkFull(game) {
+    for(let key in game.playground.fields) {
+        let field = game.playground.fields[key];
+        if(!field.get()) {
+            return false;
+        }
+    }
+
+    return true;
 }
